da9c8a3a4525f09e87ded4861443c0cd4a167817
Brad Wilson <dotnetguy@gmail.com>
2014-08-09 18:31:10 -0700

    Add support for new failure messages.

--- xunit.runner.visualstudio.testadapter/Visitors/VsExecutionVisitor.cs
+++ xunit.runner.visualstudio.testadapter/Visitors/VsExecutionVisitor.cs
@@ -39,7 +39,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
 
         protected override bool Visit(ITestFailed testFailed)
         {
-            var result = MakeVsTestResult(testFailed, TestOutcome.Failed);
+            var result = MakeVsTestResult(TestOutcome.Failed, testFailed);
             result.ErrorMessage = ExceptionUtility.CombineMessages(testFailed);
             result.ErrorStackTrace = ExceptionUtility.CombineStackTraces(testFailed);
 
@@ -51,7 +51,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
 
         protected override bool Visit(ITestPassed testPassed)
         {
-            var result = MakeVsTestResult(testPassed, TestOutcome.Passed);
+            var result = MakeVsTestResult(TestOutcome.Passed, testPassed);
             recorder.RecordEnd(result.TestCase, result.Outcome);
             recorder.RecordResult(result);
 
@@ -60,7 +60,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
 
         protected override bool Visit(ITestSkipped testSkipped)
         {
-            var result = MakeVsTestResult(testSkipped, TestOutcome.Skipped);
+            var result = MakeVsTestResult(TestOutcome.Skipped, testSkipped);
             recorder.RecordEnd(result.TestCase, result.Outcome);
             recorder.RecordResult(result);
 
@@ -74,19 +74,69 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
             return !cancelledThunk();
         }
 
-        private VsTestResult MakeVsTestResult(ITestResultMessage testResult, TestOutcome outcome)
+        protected override bool Visit(ITestAssemblyCleanupFailure cleanupFailure)
         {
-            var testCase = testCases[testResult.TestCase];
-            var fqTestMethodName = String.Format("{0}.{1}", testResult.TestCase.TestMethod.TestClass.Class.Name, testResult.TestCase.TestMethod.Method.Name);
-            var displayName = settings.GetDisplayName(testResult.TestDisplayName, testResult.TestCase.TestMethod.Method.Name, fqTestMethodName);
+            return WriteError(String.Format("Test Assembly Cleanup Failure ({0})", cleanupFailure.TestAssembly.Assembly.AssemblyPath), cleanupFailure, cleanupFailure.TestCases);
+        }
 
-            var result = new VsTestResult(testCase)
+        protected override bool Visit(ITestCaseCleanupFailure cleanupFailure)
+        {
+            return WriteError(String.Format("Test Case Cleanup Failure ({0})", cleanupFailure.TestCase.DisplayName), cleanupFailure, cleanupFailure.TestCases);
+        }
+
+        protected override bool Visit(ITestClassCleanupFailure cleanupFailure)
+        {
+            return WriteError(String.Format("Test Class Cleanup Failure ({0})", cleanupFailure.TestClass.Class.Name), cleanupFailure, cleanupFailure.TestCases);
+        }
+
+        protected override bool Visit(ITestCollectionCleanupFailure cleanupFailure)
+        {
+            return WriteError(String.Format("Test Collection Cleanup Failure ({0})", cleanupFailure.TestCollection.DisplayName), cleanupFailure, cleanupFailure.TestCases);
+        }
+
+        protected override bool Visit(ITestCleanupFailure cleanupFailure)
+        {
+            return WriteError(String.Format("Test Cleanup Failure ({0})", cleanupFailure.TestDisplayName), cleanupFailure, cleanupFailure.TestCases);
+        }
+
+        protected override bool Visit(ITestMethodCleanupFailure cleanupFailure)
+        {
+            return WriteError(String.Format("Test Method Cleanup Failure ({0})", cleanupFailure.TestMethod.Method.Name), cleanupFailure, cleanupFailure.TestCases);
+        }
+
+        protected bool WriteError(string failureName, IFailureInformation failureInfo, IEnumerable<ITestCase> testCases)
+        {
+            foreach (var testCase in testCases)
+            {
+                var result = MakeVsTestResult(TestOutcome.Failed, testCase, testCase.DisplayName);
+                result.ErrorMessage = String.Format("[{0}]: {1}", failureName, ExceptionUtility.CombineMessages(failureInfo));
+                result.ErrorStackTrace = ExceptionUtility.CombineStackTraces(failureInfo);
+
+                recorder.RecordEnd(result.TestCase, result.Outcome);
+                recorder.RecordResult(result);
+            }
+
+            return !cancelledThunk();
+        }
+
+        private VsTestResult MakeVsTestResult(TestOutcome outcome, ITestResultMessage testResult)
+        {
+            return MakeVsTestResult(outcome, testResult.TestCase, testResult.TestDisplayName, (double)testResult.ExecutionTime, testResult.Output);
+        }
+
+        private VsTestResult MakeVsTestResult(TestOutcome outcome, ITestCase testCase, string displayName, double executionTime = 0.0, string output = null)
+        {
+            var vsTestCase = testCases[testCase];
+            var fqTestMethodName = String.Format("{0}.{1}", testCase.TestMethod.TestClass.Class.Name, testCase.TestMethod.Method.Name);
+            var vsDisplayName = settings.GetDisplayName(displayName, testCase.TestMethod.Method.Name, fqTestMethodName);
+
+            var result = new VsTestResult(vsTestCase)
             {
 #if !WINDOWS_PHONE_APP
                 ComputerName = Environment.MachineName,
 #endif
-                DisplayName = displayName,
-                Duration = TimeSpan.FromSeconds((double)testResult.ExecutionTime),
+                DisplayName = vsDisplayName,
+                Duration = TimeSpan.FromSeconds(executionTime),
                 Outcome = outcome,
             };
 
@@ -94,8 +144,8 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
             if (result.Duration.TotalMilliseconds == 0)
                 result.Duration = TimeSpan.FromMilliseconds(1);
 
-            if (!String.IsNullOrEmpty(testResult.Output))
-                result.Messages.Add(new VsTestResultMessage(VsTestResultMessage.StandardOutCategory, testResult.Output));
+            if (!String.IsNullOrEmpty(output))
+                result.Messages.Add(new VsTestResultMessage(VsTestResultMessage.StandardOutCategory, output));
 
             return result;
         }
