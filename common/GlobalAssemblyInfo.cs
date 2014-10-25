49cee1b7dbae0bb63a29cebc0c6f1589c88fe58f
Brad Wilson <dotnetguy@gmail.com>
2014-10-25 10:29:21 -0600

    Update assembly version

--- common/GlobalAssemblyInfo.cs
+++ common/GlobalAssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyCompany("Outercurve Foundation")]
 [assembly: AssemblyProduct("xUnit.net Runner for Visual Studio")]
 [assembly: AssemblyCopyright("Copyright (C) Outercurve Foundation")]
-[assembly: AssemblyVersion("0.99.8.0")]
+[assembly: AssemblyVersion("0.99.9.0")]
