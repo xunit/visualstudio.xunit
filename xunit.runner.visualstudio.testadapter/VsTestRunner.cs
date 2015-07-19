65fe4cab42164052797150b5aa8bdb898e70b802
Brad Wilson <dotnetguy@gmail.com>
2015-07-19 15:34:45 -0700

    As expected, the VS runner must always use app domains to prevent assembly locking

--- xunit.runner.visualstudio.testadapter/VsTestRunner.cs
+++ xunit.runner.visualstudio.testadapter/VsTestRunner.cs
@@ -159,7 +159,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
                             {
                                 var diagnosticMessageVisitor = new DiagnosticMessageVisitor(logger, fileName, configuration.DiagnosticMessagesOrDefault);
 
-                                using (var framework = new XunitFrontController(configuration.UseAppDomainOrDefault, assemblyFileName, configFileName: null, shadowCopy: true, diagnosticMessageSink: diagnosticMessageVisitor))
+                                using (var framework = new XunitFrontController(useAppDomain: true, assemblyFileName: assemblyFileName, configFileName: null, shadowCopy: true, diagnosticMessageSink: diagnosticMessageVisitor))
                                 {
                                     var targetFramework = framework.TargetFramework;
                                     if (targetFramework.StartsWith("MonoTouch", StringComparison.OrdinalIgnoreCase) ||
@@ -330,7 +330,7 @@ namespace Xunit.Runner.VisualStudio.TestAdapter
 #endif
 
                 var diagnosticMessageVisitor = new DiagnosticMessageVisitor(logger, assemblyDisplayName, runInfo.Configuration.DiagnosticMessagesOrDefault);
-                var controller = new XunitFrontController(runInfo.Configuration.UseAppDomainOrDefault, assemblyFileName, configFileName: null, shadowCopy: true, diagnosticMessageSink: diagnosticMessageVisitor);
+                var controller = new XunitFrontController(useAppDomain: true, assemblyFileName: assemblyFileName, configFileName: null, shadowCopy: true, diagnosticMessageSink: diagnosticMessageVisitor);
 
                 lock (toDispose)
                     toDispose.Add(controller);
