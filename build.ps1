215a5cdfc267eb4cfe4b861e26a3fe6259f54be4
Oren Novotny <oren@novotny.org>
2015-08-01 17:58:17 -0700

    Add support for Windows Universal Apps (UWP)

--- build.ps1
+++ build.ps1
@@ -10,19 +10,19 @@ if ($msbuilds.Count -gt 0) {
 }
 else {
     if (test-path "env:\ProgramFiles(x86)") {
-        $path = join-path ${env:ProgramFiles(x86)} "MSBuild\12.0\bin\MSBuild.exe"
+        $path = join-path ${env:ProgramFiles(x86)} "MSBuild\14.0\bin\MSBuild.exe"
         if (test-path $path) {
             $msbuild = $path
         }
     }
     if ($msbuild -eq $null) {
-        $path = join-path $env:ProgramFiles "MSBuild\12.0\bin\MSBuild.exe"
+        $path = join-path $env:ProgramFiles "MSBuild\14.0\bin\MSBuild.exe"
         if (test-path $path) {
             $msbuild = $path
         }
     }
     if ($msbuild -eq $null) {
-        throw "MSBuild could not be found in the path. Please ensure MSBuild v12 (from Visual Studio 2013) is in the path."
+        throw "MSBuild could not be found in the path. Please ensure MSBuild v14 (from Visual Studio 2015) is in the path."
     }
 }
 
