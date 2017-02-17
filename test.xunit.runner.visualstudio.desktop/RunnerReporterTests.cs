86cec5a8b01b20ece4f37bd7e7f5ce72dfdbb8f1
Kevin Halverson <kevinh@microsoft.com>
2017-02-17 10:09:15 -0800

    Fix syntax to work with C# 6 compiler...

--- test.xunit.runner.visualstudio.desktop/RunnerReporterTests.cs
+++ test.xunit.runner.visualstudio.desktop/RunnerReporterTests.cs
@@ -8,18 +8,30 @@ public class RunnerReporterTests
 {
     public class TestRunnerReporterNotEnabled : IRunnerReporter
     {
-        string IRunnerReporter.Description => throw new NotImplementedException();
+        string IRunnerReporter.Description
+        {
+            get { throw new NotImplementedException(); }
+        }
 
         bool IRunnerReporter.IsEnvironmentallyEnabled => false;
 
-        string IRunnerReporter.RunnerSwitch => throw new NotImplementedException();
+        string IRunnerReporter.RunnerSwitch
+        {
+            get { throw new NotImplementedException(); }
+        }
 
-        IMessageSink IRunnerReporter.CreateMessageHandler(IRunnerLogger logger) => throw new NotImplementedException();
+        IMessageSink IRunnerReporter.CreateMessageHandler(IRunnerLogger logger)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class TestRunnerReporter : TestRunnerReporterNotEnabled, IRunnerReporter
     {
-        bool IRunnerReporter.IsEnvironmentallyEnabled => true;
+        bool IRunnerReporter.IsEnvironmentallyEnabled
+        {
+            get { return true; }
+        }
     }
 
     [Fact]
