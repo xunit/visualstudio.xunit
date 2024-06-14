dd2dc8b2724d0694eb9318857aa3d4a8f6a949de
Brad Wilson <dotnetguy@gmail.com>
2024-06-14 11:31:10 -0700

    Don't need folder exclusions for code that's been deleted

--- tools/builder/models/BuildContext.cs
+++ tools/builder/models/BuildContext.cs
@@ -4,10 +4,5 @@ namespace Xunit.BuildTools.Models;
 
 public partial class BuildContext
 {
-	public partial IReadOnlyList<string> GetSkippedAnalysisFolders() =>
-		new[]
-		{
-			"src/xunit.runner.visualstudio/Utility/AssemblyResolution/Microsoft.DotNet.PlatformAbstractions",
-			"src/xunit.runner.visualstudio/Utility/AssemblyResolution/Microsoft.Extensions.DependencyModel"
-		};
+	public partial IReadOnlyList<string> GetSkippedAnalysisFolders() => [];
 }
