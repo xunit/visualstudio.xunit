5b85e339bb5877117d586d36867ccbbf74dbcf1f
Brad Wilson <dotnetguy@gmail.com>
2025-07-16 13:57:13 -0700

    Copy/paste issue for running .NET tests

--- tools/builder/targets/TestCore.cs
+++ tools/builder/targets/TestCore.cs
@@ -18,7 +18,6 @@ public class TestCore
 		File.Delete(Path.Combine(context.TestOutputFolder, "test.xunit.runner.visualstudio-netcore.trx"));
 
 		await context.Exec("dotnet", $"test test/test.xunit.runner.visualstudio -tl:off --configuration {context.Configuration} --no-build --framework net8.0 --logger trx;LogFileName=test.xunit.runner.visualstudio-netcore.trx --results-directory \"{context.TestOutputFolder}\" --verbosity {context.Verbosity}");
-		await context.Exec("dotnet", $"test test/test.v2 -tl:off --configuration {context.Configuration} --no-build --framework net472 --verbosity {context.Verbosity}");
-		await context.Exec("dotnet", $"test test/test.v1 -tl:off --configuration {context.Configuration} --no-build --framework net472 --verbosity {context.Verbosity}");
+		await context.Exec("dotnet", $"test test/test.v2 -tl:off --configuration {context.Configuration} --no-build --framework net8.0 --verbosity {context.Verbosity}");
 	}
 }
